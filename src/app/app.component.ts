2827b42038cb7e4cd3e0e80a3454a96bb08ed43b
unknown <singhyuvraj1995@yahoo.com0>
2024-11-07 22:50:07 +0530

    Final push 07 Nov

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent,UserComponent],
+  imports: [HeaderComponent,UserComponent,TasksComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'NewAngularApp';
+  users = DUMMY_USERS;
+  selectedUserId?: string;
+
+  get selectedUser(){
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
+  onSelectChange(id:string){
+    //console.log('Selected user with id' + id);
+    this.selectedUserId = id;
+
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,23 +1,33 @@
-import { Component } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
-
+import { Component , EventEmitter, input, Input, Output} from '@angular/core';
+import { User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-      selectedUser = DUMMY_USERS[randomIndex] 
+      // Preferred - Way - I (With Input Decorators)
+      @Input({required:true}) user!: User;
+      @Input({required:true}) selected!: boolean;
+      @Output() select = new EventEmitter<string>();
 
       get imagePath(){
-       return 'assets/users/' + this.selectedUser.avatar
+        return 'assets/users/' + this.user.avatar;
       }
+
+      /*Way-II(Optional but not recommended because we can't change the value of the properties later) 
+      avatar = input.required<string>();
+       name = input.required<string>();
+      
+       imagePath = computed(()=>{
+          return 'assets/users/' + this.avatar();
+       })
+      */
+
       onSelectUser(){
-        const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
-        this.selectedUser = DUMMY_USERS[randomIndex];
+        this.select.emit(this.user.id)
       }
 }
